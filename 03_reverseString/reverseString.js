6a9d9314c049a6d026890a00fde1bfc752c9bc94
xpall <127076043+xpall@users.noreply.github.com>
2023-08-10 00:06:58 +0800

    Done with 03-reverseString. Note to self, read more methods for each data typegit add .!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+  let reversedText = '';
+  for (i = (text.length - 1); i >= 0; i--) {
+    //trying so hard using negative index, turns out it doesnt work in js
+    //just saw the solution, realized that my code was awful 
+    reversedText += text[i];
+  }
+  return reversedText
 };
 
 // Do not edit below this line
