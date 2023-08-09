9ca4f0ec75dd13def03d6864a12707a245e4d8b7
xpall <127076043+xpall@users.noreply.github.com>
2023-08-09 23:28:11 +0800

    Done with 02-repeatString (reinforced that str.concat does not update the value, use += operator instead, and mind the return statements

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
-
+const repeatString = function(text, repetition) {
+  return checkRepetition(text, repetition)
 };
+
+function checkRepetition(text, repetition) {
+  if (repetition >= 0) {  
+    return repeatTextNTimes(text, repetition);  
+  } else if (repetition < 0) {
+    return 'ERROR';
+  }
+}
+
+function repeatTextNTimes(text, repetition) {
+  let repeatedText = '';
+  for (i = +repetition; i > 0; i--) {
+    // repeatedText = repeatedText.concat(text); //string is immutable
+    repeatedText += text;
+  }
+  return repeatedText;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
