e3e8660742e7712af1eff27166d177e66f5c4be8
xpall <127076043+xpall@users.noreply.github.com>
2023-08-11 00:46:12 +0800

    Done with 08-calculator. Note-to-self: Ok in for loop

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return (x + y);
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return (x - y);
 };
 
-const sum = function() {
-	
+const sum = function(someArray) {
+	let answer = 0;
+  for (n of someArray) {
+    answer += n;
+  }
+  return answer;
 };
 
-const multiply = function() {
-
+const multiply = function(someArray) {
+	let answer = 1;
+  for (n of someArray) {
+    answer *= n;
+  }
+  return answer;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return (x ** y);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let answer = 1;
+  for (i = n; n > 0; n--) {
+    answer *= n;
+  }
+  return answer;
 };
 
 // Do not edit below this line
