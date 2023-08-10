fd46d847bf5078c20216ac1f2237147bc160a3a2
xpall <127076043+xpall@users.noreply.github.com>
2023-08-10 22:58:47 +0800

    Done with 05-sumAll. Note-to-self: Adjacent parenthesis != multiplication. Practice more on breaking down code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
 const sumAll = function() {
+  numArray = [];
 
+// check if arguments are numbers
+// if not, push to the array
+for (i = 0; i < (arguments.length); i++) {
+  if (typeof arguments[i] === 'number' && arguments[i] >= 1) {
+    numArray.push(arguments[i]);
+  } else {
+    return 'ERROR';
+  } 
+}
+  // Sum of Arithmetic Sequence Formula
+  // S = ((n/2))(a + L) // n=totalDigits, a=firstTerm, L=lastTerm
+  let sortednumArray = numArray.sort();
+  let sum = (((sortednumArray[1] - sortednumArray[0]) + 1) / 2) 
+            * (sortednumArray[0] + sortednumArray[1]);
+  return sum;
 };
 
 // Do not edit below this line
