3b67919e6d6996851eec95ae1f71b4918693faca
xpall <127076043+xpall@users.noreply.github.com>
2023-08-11 00:24:00 +0800

    Done with 07-tempCoversion. Note-to-self: math.round work-around, .toFixed does not work on math expression, use on variable name instead.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,29 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let convertedTemp = ((5 / 9) *( temp - 32));
+  let roundedOff = Math.round(convertedTemp * 10) / 10;
+  return roundedOff;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let convertedTemp = (((9 / 5) * temp) + 32);
+  let roundedOff = Math.round(convertedTemp * 10)/10;
+  return roundedOff;
+
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = {
