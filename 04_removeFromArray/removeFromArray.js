10c2056888f40699a7d00230111ad95ba17cc5a2
xpall <127076043+xpall@users.noreply.github.com>
2023-08-10 21:41:45 +0800

    Done with 04-removeFromArrays, self-lesson: reinforced arguments inside functions, and ! operator to reverse boolean

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
 const removeFromArray = function() {
+  let templateArray = Array.from(arguments[0]);
+  let removeArray = [];
+  for (let i = 1; i < (arguments.length); i++) {
+    removeArray.push(arguments[i]);
+  }
+  // console.log(templateArray);
+  // console.log(removeArray);
+  finalArray = templateArray.filter(function(el) {
+    return !removeArray.includes(el);
+  });
+  return finalArray;
 
-};
 
+// split any arguments to two groups (one for array, one for to-be-removed)
+// loop through the array, for each element
+
+
+}
 // Do not edit below this line
 module.exports = removeFromArray;
