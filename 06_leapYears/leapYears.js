2fa6c2fefe744bfb6c64185050a0c6e397c6b367
xpall <127076043+xpall@users.noreply.github.com>
2023-08-10 23:58:09 +0800

    Done with 06-leapYears. Note-to-self: analyze the problem first!

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  return isLeapYear(year);
+};
 
+function isLeapYear(yyyy) {
+// divisible by 4 is a leap year
+// unless it is divisible by 100 not a leap year
+// except divisible by 400
+  if (yyyy % 400 === 0 || yyyy % 100 != 0 && yyyy % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
