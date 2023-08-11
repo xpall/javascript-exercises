8127f4109577878207b1349d34af4836ff6d6640
xpall <127076043+xpall@users.noreply.github.com>
2023-08-11 11:06:19 +0800

    Done with 10-fibonacci. Note-to-self: Array has no negative index, use arr.length - n instead. Array is mutable using some methods

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if (isValidNumber(n) === true) {
+    return fibonacciTotal(n);
+  } else {
+    return 'OOPS'
+  }
+  
+function isValidNumber(n) {
+  return (n >= 0);
+}
 
+function fibonacciTotal(x){
+  const fibonacciStart = [1, 1]
+  for (i = 2; i < x; i++) {
+    let lastTwoDigits = [fibonacciStart[(fibonacciStart.length - 1)], 
+                        fibonacciStart[(fibonacciStart.length - 2)]];
+    fibonacciStart.push(lastTwoDigits[0] + lastTwoDigits[1]);
+  } return fibonacciStart.pop();
+}
 };
 
 // Do not edit below this line
