e4477dfe85d044bb9820693281775598c5171253
xpall <127076043+xpall@users.noreply.github.com>
2023-08-11 11:11:29 +0800

    Done with 11-getTheTitles. Note-to-self: Recall that indexing a dict use the key not the index number.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(dict) {
+  bookTitles = [];
+  console.log(dict.length)
+  for (books of dict) {
+    bookTitles.push(books['title'])
+  }
+  return bookTitles;
 };
 
 // Do not edit below this line
