748816d497a2649a84ff39c10085c9666412dc00
xpall <127076043+xpall@users.noreply.github.com>
2023-08-11 10:31:11 +0800

    Done with 09-palindromes. Note-to-self: Always practice regex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (someString) {
+  return isPalindrome(cleanString(someString));
 };
+
+function cleanString(str) {
+  return str.toLowerCase().replace(/[^a-zA-Z0-9]/g,'');
+}
+
+function isPalindrome(fstr) {
+  let reverseFstring = fstr.split('').reverse('').join('');
+  return (fstr === reverseFstring);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
