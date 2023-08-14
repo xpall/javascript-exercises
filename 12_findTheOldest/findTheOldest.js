81a54c1a0ff9822f5c7723a25847828977a4f286
xpall <127076043+xpall@users.noreply.github.com>
2023-08-14 21:32:12 +0800

    array.reduce is kinda hard. Note-to-self: Understand .spec.js file FIRSTgit add .!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArray) {
+  return appendAge(peopleArray);
 };
+
+// update array (add age, take note of people that are still living)
+function appendAge(someArray) {
+  let currentYear = 2023;
+  oldestPerson = {name: '', age: 0};
+  for (i = 0; i < someArray.length; i++) {
+    // is still living
+    if (!someArray[i]['yearOfDeath']) {
+      someArray[i]['yearOfDeath'] = currentYear;
+    };
+    personAge = someArray[i]['yearOfDeath'] - someArray[i]['yearOfBirth'];
+    personName = someArray[i]['name'];
+    // if oldest
+    if (personAge > oldestPerson['age']) {
+      oldestPerson['age'] = personAge;
+      oldestPerson['name'] = personName;
+    }
+  } 
+  return oldestPerson;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
